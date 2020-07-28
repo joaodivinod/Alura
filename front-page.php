72c411658477c5549a3c8fbdb2b2c70be6bac242
JOAO DIVINO DA SILVA <60980853+joaodivinod@users.noreply.github.com>
2020-07-28 16:42:26 -0300

    aula 6 concluida MetaBox

---
+++ front-page.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'header.php';
+?>
+
+<h1>Home</h1>
+<?php
+require_once 'footer.php';
+?>
--- page-destinos.php
+++ page-destinos.php
@@ -58,4 +58,22 @@ if ($query->have_posts()):
     echo '</ul>';
     echo '</main>';
 endif;
+$args = array(
+    'post_type' => 'banners',
+    'post_status' => 'publish',
+    'posts_per_page' => 1
+);
+
+$query = new WP_Query($args);
+if($query->have_posts()):
+while($query->have_posts()): $query->the_post();
+?>
+<main>
+    <div class="imagem-banner">
+        <?php the_post_thumbnail(); ?>
+    </div>
+</main>
+<?php
+endwhile;
+endif;
 require_once 'footer.php';
